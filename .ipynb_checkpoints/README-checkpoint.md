fc75b73dea09d3c25c5c1971e3dd5577d835532f
johan <castrogaray-j@webmail.uwinnipeg.ca>
2025-04-25 11:46:05 -0500

    Final commit

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,29 @@
+# EEG-Based Classification of Driving Performance through Spectrogram Analysis
+Maintaining optimal driving performance is critical for road safety, as it depends
+on various cognitive factors, including attention, reaction time, and decisionmaking.
+Cognitive load and attentional shifts influence driving behavior, and
+EEG-based analysis offers a promising method to assess these cognitive states
+in real-time. While previous research has explored EEG for attention detection,
+its direct correlation with driving performance remains underdeveloped.
+Driving performance is influenced by multiple factors, including environmental
+conditions, driver fatigue, and cognitive workload. Traditional methods for
+assessing driver performance, such as behavioral observation and self-reports,
+are often subjective and lack real-time precision. By utilizing EEG, it is possible
+to gain insights into neural activity that correlates with cognitive states
+affecting driving efficiency. The analysis of EEG data provides a time-frequency
+representation that allows for a more detailed investigation of transient neural
+patterns associated with driving behaviors.
+Additionally, modern driving assistance systems rely heavily on external sensors,
+such as cameras and radar, to monitor driver behavior. However, these
+systems do not account for internal cognitive states that may impact reaction
+time and decision-making. By integrating EEG-based classification models into
+these systems, it is possible to develop more comprehensive driver monitoring
+technologies that enhance road safety and mitigate accident risks.
+This project aims to classify driving performance using spectrograms images extracted from EEG data using machine
+learning techniques. By analyzing the spectrograms and associated behavioral
+responses in a driving simulator environment, we seek to develop a robust classification
+model to differentiate between high and low driving performance states.
+The outcomes of this study will contribute to cognitive neuroscience and intelligent
+transport systems by enabling real-time driver state monitoring, ultimately
+improving driver safety, reducing accidents, and advancing adaptive driver assistance
+technologies.
---
+++ .ipynb_checkpoints/test_ERDS-checkpoint.py
@@ -0,0 +1,193 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import seaborn as sns
+import pickle
+import mne
+import gc
+import concurrent.futures
+import multiprocessing
+
+from mne.channels import make_1020_channel_selections
+from mne.event import define_target_events
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+
+
+from sklearn.model_selection import KFold, train_test_split, cross_validate
+from sklearn.metrics import precision_recall_curve, roc_curve, roc_auc_score
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.dummy import DummyClassifier
+import matplotlib
+matplotlib.use('Agg')
+
+class_names = ['1','2','3']
+
+bars = [None] * 56
+
+os.chdir('/home/castrogaray-j/U_Winnipeg_OneDrive/Pattern recognition/BICT/Events_raw')
+
+files = os.listdir()
+pkl_files = sorted([file for file in files if file.startswith('raw') and file.endswith('.pkl')])
+subjects = set([file[4:6] for file in pkl_files])
+
+with open(pkl_files[0], 'rb') as file:
+    events = pickle.load(file)
+
+#%%capture --no-stderr --no-stdout
+wind_length = 4
+b_freq, u_freq = 1.0, 50.0
+sfreq = events[0].info['sfreq']
+freqs = np.arange(b_freq, u_freq, 1)  # Frequency range
+n_cycles = freqs / 2.  # Number of cycles per frequency
+
+ch_names = events[0].info['ch_names']
+
+#total_iter= len(pkl_files)
+#progress_bar = tqdm(total=total_iter, desc="Progress", unit="iter")
+
+def plot_spectrogram(power, file_path):
+    import matplotlib.pyplot as plt
+    # Extraer datos
+    times_plt = power.times
+    freqs_plt = power.freqs
+    data_plt = power.data[ 0, :, :]  # Seleccionar el canal correspondiente
+    
+    # Aplicar baseline desde el inicio hasta t=0
+    baseline_idx = np.where(times_plt <= 0)[0]  # Índices donde el tiempo es <= 0
+    baseline_mean = np.mean(data_plt[:, baseline_idx], axis=1, keepdims=True)  # Promedio en baseline
+    
+    # Aplicar corrección de baseline según 'logratio'
+    data_plt = np.log10(data_plt / baseline_mean)
+    
+    # Configuración de límites de color
+    vmin, vmax = np.percentile(data_plt, [5, 95])  # MNE ajusta entre percentiles 5 y 95 por defecto
+    
+    
+    # Crear la figura
+    fig, axes = plt.subplots(figsize=(10, 5))
+    pcm = plt.pcolormesh(times_plt, freqs_plt, data_plt, shading='auto', cmap='RdBu_r', vmin=vmin, vmax=vmax)
+    
+    for ax in fig.axes:
+                ax.set_xticks([])  # Quitar ticks en eje X
+                ax.set_yticks([])  # Quitar ticks en eje Y
+                ax.set_xlabel("")  # Quitar etiqueta del eje X
+                ax.set_ylabel("")  # Quitar etiqueta del eje Y
+                ax.set_frame_on(False)  # Quitar borde del gráfico
+
+    plt.savefig(file_path, dpi=100, bbox_inches='tight')
+    
+    # Cerrar la figura para liberar memoria
+    plt.close(fig)
+    #del fig, axes, plt, pcm
+    #gc.collect()
+
+def plot_erds(power, file_path):
+        import matplotlib.pyplot as plt
+
+        # Extract data
+        times = power.times
+        freqs = power.freqs
+        data = power.data[0, 0, :, :]  # Select the corresponding channel
+
+        # Apply baseline correction
+        baseline = (0,1)
+        power.apply_baseline(baseline=baseline, mode='percent', verbose='error')
+
+        # Plot ERDS map
+        fig, ax = plt.subplots(figsize=(10, 5))
+        cax = ax.imshow(data, aspect='auto', origin='lower', 
+                        extent=[times[0], times[-1], freqs[0], freqs[-1]], 
+                        cmap='RdBu_r', vmin=-1, vmax=1)
+        # ax.set_title(title)
+        # ax.set_xlabel('Time (s)')
+        # ax.set_ylabel('Frequency (Hz)')
+        # fig.colorbar(cax, ax=ax, label='ERDS (dB)')
+        # for ax in fig.axes:
+        for ax in fig.axes:
+                ax.set_xticks([])  # Quitar ticks en eje X
+                ax.set_yticks([])  # Quitar ticks en eje Y
+                ax.set_xlabel("")  # Quitar etiqueta del eje X
+                ax.set_ylabel("")  # Quitar etiqueta del eje Y
+                ax.set_frame_on(False)  # Quitar borde del gráfico
+            
+        plt.savefig(file_path, dpi=100, bbox_inches='tight', pad_inches=0.0)
+
+with open('event_labels/performance_labels', 'rb') as file:
+     labels_array = pickle.load(file)
+
+def ICA_components(raw_cleaned):
+    ica_cleaned = mne.preprocessing.ICA(
+        n_components=64, method="picard", max_iter="auto", random_state=97, verbose='error'
+    )
+    
+    ica_cleaned.fit(raw_cleaned, verbose='error')
+
+    components = ica_cleaned.get_sources(raw_cleaned)
+    return components
+    
+def procesar_archivo(idx):
+    """
+    Función que procesa un archivo en paralelo
+    """
+    pkl = pkl_files[idx]  # Selecciona el archivo correspondiente
+    labels_df = labels_array[idx]  # Obtiene los labels correspondientes
+    
+    with open(pkl, 'rb') as file:
+        events = pickle.load(file)
+
+    filtered_events = [item for i, item in enumerate(events[1:]) if i in labels_df.index]
+    total_iter= len(filtered_events)
+    bars[idx] = tqdm(total=total_iter, desc="Progress", unit="iter")
+
+    raw = mne.concatenate_raws(events)
+    epochs = mne.make_fixed_length_epochs(raw, duration=5, preload=True, verbose='error')
+    components = ICA_components(epochs)
+    #print("\n")
+    #print(int(pkl[8:10]), int(pkl[-5]))
+    num_events = components.get_data().shape[0]
+    for event_idx in range(num_events):
+        #print( event_idx, int(pkl[8:10]), int(pkl[-5]))
+        #event.crop(tmin=1)
+        componets = components[event_idx]
+        label = labels_df['label'].values[event_idx]
+        
+        ch_names = componets.info['ch_names']  # Obtener nombres de canales
+        for ch in ch_names:
+            power = componets.compute_tfr(method="multitaper", freqs=freqs, n_cycles=n_cycles, picks=ch)
+            path = "/data/castrogaray-j/test_EDRS/subject_{}_class_{}_label_{}_event_{}_channel_{}.png".format(int(pkl[8:10]), int(pkl[-5]), label, event_idx, ch)
+            #plot_spectrogram(power, path)
+            plot_erds(power, path)
+        #del power
+        #gc.collect()
+
+        bars[idx].update(1)
+
+    # Cerrar la barra de progreso al finalizar
+    bars[idx].close()
+
+
+if __name__ == "__main__":
+    num_procesos = min(56, len(pkl_files))
+    print(num_procesos)
+
+    parametros = list(range(num_procesos))  # Lista de parámetros del 0 al 55
+    
+    
+    
+    # Crear un pool de procesos
+    # with multiprocessing.Pool(processes=num_procesos) as pool:
+    #     pool.map(procesar_archivo, range(num_procesos))
+    with multiprocessing.Pool(processes=num_procesos) as pool:
+        pool.map(procesar_archivo, range(len(pkl_files)))  # Enviar solo índices válidos
